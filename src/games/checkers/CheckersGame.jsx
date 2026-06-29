6d2c1917941ab842676f84f4a897cbbbe92f49c4
sssnssjsjjs@gmail.com <sssnssjsjjs@gmail.com>
2026-06-29 16:54:56 +0300

    gu

--- src/games/checkers/CheckersGame.jsx
+++ src/games/checkers/CheckersGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { doc, updateDoc } from 'firebase/firestore';
 import { playSound } from '../../utils/sound.js';
 import { getValidCheckersMoves, checkCheckersWinner, createInitialCheckersBoard } from './logic.js';
